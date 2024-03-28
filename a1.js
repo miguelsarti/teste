27bf575303dcb4f9a519726e1cc4eaa53aabadbe
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-03-28 08:34:52 -0300

    teste

---
+++ a1.js
@@ -0,0 +1,6 @@
+let a = 19;
+if(a >= 18){
+    console.log("Você tem idade suficiente para dirigir,", a);
+}else{
+    console.log("Você não tem idade suficiente para dirigir,", a);
+}
---
+++ teste.js
@@ -0,0 +1,6 @@
+let conjuntos = [3, 5, 4, 8, 13, 16];
+
+conjuntos.sort((a, b) => a - b);
+
+let par = [];
+let impar = [];
